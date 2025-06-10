9fbfba0f2465ed74a6b2aae9d2a962c0ded0883d
Taylo <Taylo@MARIEL>
2025-06-10 14:34:17 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// find the positive number from list of nums -LINQ QUERY
+using System.Threading.Channels;
+
+namespace LINQAssignment
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, -1, 3, -3, 10, -200 };
+            var positivenumbers = from num in nums
+                                  where num > 0
+                                  select num;
+            foreach (var num in positivenumbers)
+            {
+
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
